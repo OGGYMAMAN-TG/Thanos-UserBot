7d3fe0462c838c4b9c583c7c8b04f796a78d7a71
RISHABH ANANAD <94896142+thanosuser@users.noreply.github.com>
2022-01-19 10:24:44 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -54,6 +54,62 @@ import userbot._core
 
 print(f"""Hello sir i am THANOS!! THANOS VERSION :- {THANOSversion} YOUR THANOS IS READY! FOR CHECK YOUR BOT WORKING OR NOT PLEASE TYPE (.thanos/.ping) ENJOY YOUR BOT! JOIN FOR MORE FUTURE UPDATES @thanosBot_chats .""")
 
+
+
+    try:
+        await bot(JoinChannelRequest("@thanos_userbots"))
+    except BaseException:
+        pass
+    try:
+        if H2:
+            await H2(JoinChannelRequest("@thanos_userbots"))
+    except BaseException:
+        pass
+    try:
+        if H3:
+            await H3(JoinChannelRequest("@thanos_userbots"))
+    except BaseException:
+        pass
+    try:
+        if H4:
+            await H4(JoinChannelRequest("@thanos_userbots"))
+    except BaseException:
+        pass
+    try:
+        if H5:
+            await H5(JoinChannelRequest("@thanos_userbots"))
+    except BaseException:
+        pass
+# Why not come here and chat??
+    try:
+        await bot(ImportChatInviteRequest('thanosbot_chats'))
+    except BaseException:
+        pass
+    try:
+        if H2:
+            await H2(ImportChatInviteRequest('thanosbot_chats'))
+    except BaseException:
+        pass
+    try:
+        if H3:
+            await H3(ImportChatInviteRequest('thanosbot_chats'))
+    except BaseException:
+        pass
+    try:
+        if H4:
+            await H4(ImportChatInviteRequest('thanosbot_chats'))
+    except BaseException:
+        pass
+    try:
+        if H5:
+            await H5(ImportChatInviteRequest('thanosbot_chats'))
+    except BaseException:
+        pass
+
+
+
+
+
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
